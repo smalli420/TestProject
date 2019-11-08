983e8c6c77cb2780bd2fff64f2881798d858ef6d
smallikarjuna33 <smallikarjuna1996@gmail.com>
2019-11-08 14:22:36 +0530

    Added comments in sampleTest_Git class

--- TestProject/src/com/sampleTest_Git.java
+++ TestProject/src/com/sampleTest_Git.java
@@ -9,26 +9,30 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class sampleTest_Git {
-  WebDriver driver;
+ public static WebDriver driver;
   @Test
-  public void guru99() {
+  public static void main(String[] args){
 	  System.setProperty("webdriver.gecko.driver", "C:\\Users\\admin\\Downloads\\Selenium_setup\\geckodriver.exe");
 		driver=new FirefoxDriver();
+		//Launch the URL
 		driver.get("http://www.demo.guru99.com/V4/");
+		//Maximize the window
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);  
   
-
+     //Login Section
 	 driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr232019"); 
 	 driver.findElement(By.xpath("//input[@name='password']")).sendKeys("ubEvyvu"); 
 	 driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
 
-  
+      //Scroll the page
 	  JavascriptExecutor je=(JavascriptExecutor)driver;
 	  je.executeScript("window.scrollBy(0,500)");
+	  //Click on Logout button
 	  driver.findElement(By.linkText("Log out")).click();
 	  String s=driver.switchTo().alert().getText();
 	  System.out.println(s);
+	  //Handle alert popup
 	  driver.switchTo().alert().accept();
 	  driver.close();
   }
